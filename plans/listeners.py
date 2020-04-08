d39ff8bbeddae26906433d67f277990a9f5e8497
Marek Aleksander <44878354+Mrooie@users.noreply.github.com>
2020-04-09 01:31:37 +0200

    Kill invoicing :>

--- plans/listeners.py
+++ plans/listeners.py
@@ -8,25 +8,25 @@ from plans.signals import order_completed, activate_user_plan
 User = get_user_model()
 
 
-@receiver(post_save, sender=Order)
-def create_proforma_invoice(sender, instance, created, **kwargs):
-    """
-    For every Order if there are defined billing_data creates invoice proforma,
-    which is an order confirmation document
-    """
-    if created:
-        Invoice.create(instance, Invoice.INVOICE_TYPES['PROFORMA'])
+# @receiver(post_save, sender=Order)
+# def create_proforma_invoice(sender, instance, created, **kwargs):
+#     """
+#     For every Order if there are defined billing_data creates invoice proforma,
+#     which is an order confirmation document
+#     """
+#     if created:
+#         Invoice.create(instance, Invoice.INVOICE_TYPES['PROFORMA'])
 
 
-@receiver(order_completed)
-def create_invoice(sender, **kwargs):
-    Invoice.create(sender, Invoice.INVOICE_TYPES['INVOICE'])
+# @receiver(order_completed)
+# def create_invoice(sender, **kwargs):
+#     Invoice.create(sender, Invoice.INVOICE_TYPES['INVOICE'])
 
 
-@receiver(post_save, sender=Invoice)
-def send_invoice_by_email(sender, instance, created, **kwargs):
-    if created:
-        instance.send_invoice_by_email()
+# @receiver(post_save, sender=Invoice)
+# def send_invoice_by_email(sender, instance, created, **kwargs):
+#     if created:
+#         instance.send_invoice_by_email()
 
 
 @receiver(post_save, sender=User)
